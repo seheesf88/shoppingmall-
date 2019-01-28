ed18e727bbaf8a94c880028f0b10cc694bf3615e
Akshay199456 <akshay.kum94@gmail.com>
2019-01-28 14:54:33 -0800

    SignIn page full functionality complete

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,106 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/user")
 
 router.get("/login", (req,res) =>{
 	try{
-		res.render("index.ejs");
+		res.render("index.ejs", {
+			message : req.session.message
+		});
 	}
+	catch(err){
+		res.send(err);
+	}
+});
+
+
+router.post("/login", async (req, res) => {
+	try{
+
+		// If user selects the option to register his account, you want to collect his 
+		// info and feed that info into the database 
+		if(req.body.registerEmail){
+			
+			console.log("Successfully created an account");
+			// Converting password to hashed password
+			const password = req.body.registerPassword;
+			const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+		
+
+			// Creating user object 
+			// console.log(req.body);
+			const userObject = {};
+			userObject.firstName = req.body.registerFN;
+			userObject.lastName = req.body.registerLN;
+			userObject.email = req.body.registerEmail;
+			userObject.hashedPassword = hashedPassword;
+			userObject.admin = false;
+			userObject.itemsBrowsed = [];
+			userObject.itemsBought = [];
+		
+
+			// Storing user object into the database
+			const createdUser = await User.create(userObject);
+			console.log("createdUser " + createdUser);
+
+
+			// Storing current user email and loggedProperty in session and redirect them to the home page
+			req.session.email = userObject.email;
+			req.session.logged = true;
+			req.session.message = " ";
+			res.redirect("/auths/homePage");
+		}
+
+
+
+		// If user selects the option to login to his account, they have to be redirected to
+		// the home page on successful login
+		else{
+			const foundUser = await User.findOne({"email": req.body.loginEmail});
+			
+
+			if(foundUser){
+				// If user enters proper login details, redirect them to the home page after storing their info into session object
+				if(bcrypt.compareSync(req.body.loginPassword, foundUser.hashedPassword)){
+					console.log("loggedIn" +foundUser);
+					req.session.email = req.body.loginEmail;
+					req.session.logged = true;
+					req.session.message = " ";
+					res.redirect("/auths/homePage");
+				}
+
+
+				// If wrong password is entered, show generic error message and redirect to login page
+				else{
+					req.session.message = " Incorrect email or password entered. If you don't have an account, please register."
+					res.redirect("/auths/login");
+				}
+			}
+
+
+			// If user is not found, show generic error message and redirect them to login page
+			else{
+				req.session.message = " Incorrect email or password entered. If you don't have an account, please register."
+				res.redirect("/auths/login");
+			}
+		}
+	}
+
+	catch(err){
+		req.session.message = "Email already exists. Please create an account with a new email";
+		res.redirect("/auths/login");
+
+		// res.send(err);
+	}
+});
+
+
+router.get("/homePage", (req, res) => {
+	try{
+		res.render("homePage.ejs");
+	}
+
 	catch(err){
 		res.send(err);
 	}
