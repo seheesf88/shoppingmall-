2482c535404b35832e29b5c7b43886d0ab76bf46
Owner <owner@Owners-MacBook-Air.local>
2019-01-29 08:56:16 -0800

    admin

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const Item = require('../models/item');
+const User = require('../models/user');
 
 router.get("/homePage", (req,res) => {
 	try{
@@ -11,4 +13,63 @@ router.get("/homePage", (req,res) => {
 	}
 });
 
+// router.get('/', (req, res) =>{
+// 	Item.find({}, (err, allItems) => {
+// 		res.render('admin/index.ejs', {
+// 			items : allItems
+// 		})
+// 	})
+// })
+
+router.get('/new', (req, res) => {
+	res.render('admin/new.ejs')
+})
+
+router.get('/:id', (req, res) => {
+	Item.findById(req.params.id, (err, foundItem) => {
+		if(err){
+			res.send(err)
+		}else{
+			res.render('admin/show.ejs', {
+				item : foundItem
+			})
+		}
+	})
+})
+
+router.get(':/id/edit', (req, res) => {
+	Item.findById(req.params.id, (err, foundItem) => {
+		res.render('admin/edit.ejs',{
+			item : foundItem
+		})
+	})
+})
+
+router.post('/', (req, res) => {
+	Item.create(req.body, (err, createdItem) => {
+		if(err){
+			res.send(err)
+		}else{
+			res.redirect('/admin')
+		}
+	})
+})
+
+router.put('/:id', (req, res) => {
+	Item.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedItem) => {
+		if(err){
+			res.send(err)
+		}else{
+			res.redirect('/admin')
+		}
+	})
+})
+
+// router.delete('/:id', (req, res) => {
+// 	Item.findByIdAndRemove(req.params.id, (err, deletedItem) => {
+// 		const userIds = [];
+//
+//
+// 	})
+// })
 module.exports = router;
