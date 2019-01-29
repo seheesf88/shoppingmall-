bf79ec836c629cccbb01b8b3cf0697743fd1d88d
Akshay199456 <akshay.kum94@gmail.com>
2019-01-28 19:17:17 -0800

    Added navbar

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/user")
 
 
-router.get("/homePage", (req,res) => {
+
+router.get("/homePage", async (req,res) => {
 	try{
-		res.render("homePage.ejs");
+
+		// Render the home page for the user 
+		const foundUser = await User.findOne({"email": req.session.email});
+		console.log(foundUser);
+
+		res.render("homePage.ejs", {
+			foundUser
+		});
 	}
 
 	catch(err){
